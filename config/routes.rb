89819b0bc1c4a5fa5f4dbef1329fdf37ddb05887
Karine Legault <karine.legault.mallette14@gmail.com>
2021-02-13 14:54:39 -0500

    able to display the tasks as links

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
   # new task
   get '/tasks/new', to: 'tasks#new' # display the form
   # read all task
-  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id', to: 'tasks#show', as: "index"
   # create a task
-  post '/tasks/', to: 'tasks#create'
+  # post '/tasks/', to: 'tasks#create'
   # update a task
   get '/tasks/:id/edit', to: 'tasks#edit'
-  patch '/tasks/:id', to: 'tasks#update'
+
+  # patch '/tasks/:id', to: 'tasks#update'
   # delete a task
   delete '/tasks/:id', to: 'tasks#destroy'
 end

acef859388fd292f77b414adc5eacb67c60eb84b
Karine Legault <karine.legault.mallette14@gmail.com>
2021-02-13 16:49:37 -0500

    all good except edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to index_path(t)
+    redirect_to index_path
   end
   def edit
     @task = Task.find(params[:id])
@@ -19,12 +19,12 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task = update.(task_params)
-    redirect_to index_path(t)
+    redirect_to show_path(t)
   end
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-    redirect_to tasks_path
+    redirect_to index_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # verb 'path', to: 'controller#action'
-  get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: "index"
   # new task
   get '/tasks/new', to: 'tasks#new', as: "new" # display the form
   # read all task
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   # create a task
   post '/tasks/', to: 'tasks#create'
   # update a task
-  get '/tasks/:id/edit', to: 'tasks#edit'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: "edit"
 
   patch '/tasks/:id', to: 'tasks#update'
   # delete a task
-  delete '/tasks/:id', to: 'tasks#destroy'
+  delete '/tasks/:id', to: 'tasks#destroy', as: "delete"
 end

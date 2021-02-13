3a200a37b16ee640be96779ba9f8aa5b475fdec5
Karine Legault <karine.legault.mallette14@gmail.com>
2021-02-13 13:57:27 -0500

    able to display the tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,17 @@
 class TasksController < ApplicationController
-  def allTasks
-
+  def index
+    @tasks = Task.all
+  end
+  def show
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "tasks", to: "tasks#tasks"
+  # verb 'path', to: 'controller#action'
+  get '/tasks', to: 'tasks#index'
+  # read all task
+  get '/tasks/:id', to: 'tasks#show'
+  # create a task
+  get '/tasks/new', to: 'tasks#new' # display the form
+  post '/tasks/', to: 'tasks#create'
+  # update a task
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  # delete a task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end

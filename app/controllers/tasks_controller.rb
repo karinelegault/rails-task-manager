9143f7c14101f28299e346d58b09997ae8458483
Karine Legault <karine.legault.mallette14@gmail.com>
2021-02-13 14:38:11 -0500

    working on new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,10 +2,11 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+  def new
+
+  end
   def show
     @task = Task.find(params[:id])
-  end
-  def new
   end
   def create
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # verb 'path', to: 'controller#action'
   get '/tasks', to: 'tasks#index'
+  # new task
+  get '/tasks/new', to: 'tasks#new' # display the form
   # read all task
   get '/tasks/:id', to: 'tasks#show'
   # create a task
-  get '/tasks/new', to: 'tasks#new' # display the form
   post '/tasks/', to: 'tasks#create'
   # update a task
   get '/tasks/:id/edit', to: 'tasks#edit'

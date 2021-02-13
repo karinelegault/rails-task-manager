bdb89f4c37f351f22740c4248b9b69a67fbb913a
Karine Legault <karine.legault.mallette14@gmail.com>
2021-02-13 16:28:18 -0500

    almost done, still have issues for edit link

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to index_path(@task)
+    redirect_to index_path(t)
   end
   def edit
     @task = Task.find(params[:id])
@@ -19,7 +19,7 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task = update.(task_params)
-    redirect_to index_path(@task)
+    redirect_to index_path(t)
   end
   def destroy
     @task = Task.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   # verb 'path', to: 'controller#action'
   get '/tasks', to: 'tasks#index'
   # new task
-  get '/tasks/new', to: 'tasks#new' # display the form
+  get '/tasks/new', to: 'tasks#new', as: "new" # display the form
   # read all task
-  get '/tasks/:id', to: 'tasks#show', as: "index"
+  get '/tasks/:id', to: 'tasks#show', as: "show"
   # create a task
   post '/tasks/', to: 'tasks#create'
   # update a task
   get '/tasks/:id/edit', to: 'tasks#edit'
 
-  # patch '/tasks/:id', to: 'tasks#update'
+  patch '/tasks/:id', to: 'tasks#update'
   # delete a task
   delete '/tasks/:id', to: 'tasks#destroy'
 end
